9c5f3a8354c10257abd154d3c9f7d0a00d165d53
Ahmet Onder Mogol <aomogol@gmail.com>
2021-11-05 12:32:37 +0300

    Update cpu-belirleme.sh

--- cpu-belirleme.sh
+++ cpu-belirleme.sh
@@ -6,12 +6,12 @@
 proc_type=$(lscpu | awk '/Vendor ID:/ {print $3}')
 case "$proc_type" in
 	GenuineIntel)
-		print "Installing Intel microcode"
+		echo "Installing Intel microcode"
 		#pacman -S --noconfirm intel-ucode
 		#proc_ucode=intel-ucode.img
 		;;
 	AuthenticAMD)
-		print "Installing AMD microcode"
+		echo "Installing AMD microcode"
 		#pacman -S --noconfirm amd-ucode
 		#proc_ucode=amd-ucode.img
 		;;
